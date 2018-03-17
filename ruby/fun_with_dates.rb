8d0561068c6251d3b78554a131ff6237e8c46b73
Greg Cockburn <gergnz@gmail.com>
2018-03-17 14:55:59 +1100

    don't use date

--- ruby/fun_with_dates.rb
+++ ruby/fun_with_dates.rb
@@ -6,17 +6,46 @@
 # Not the start date, not the end.
 #
 #
-require 'date'
 
-d1 = Date.parse(ARGV[0])
-d2 = Date.parse(ARGV[1])
+MONTHS = [
+  0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
+].freeze
 
-d = if d2 > d1
-      (d2 - d1).to_i - 1
-    else
-      (d1 - d2).to_i - 1
-    end
+def monthstodays(month)
+  days = 0
+  (1...month).each do |a|
+    days += MONTHS[a]
+  end
+  days
+end
 
-d = 0 if d.negative?
+def yearstodays(year)
+  days = (year - 1900) * 365
+  days + (year - 1900) / 4
+end
 
-puts d
+def leapdays(year1, year2)
+  if year2 > year1
+    ((year1 - 1900) / 4) - ((year2 - 1900) / 4)
+  else
+    ((year2 - 1900) / 4) - ((year1 - 1900) / 4)
+  end
+end
+
+date1 = ARGV[0].split('/')
+date1 = date1.map(&:to_i)
+date2 = ARGV[1].split('/')
+date2 = date2.map(&:to_i)
+
+days1 = yearstodays(date1[2]) + monthstodays(date1[1]) + date1[0]
+days2 = yearstodays(date2[2]) + monthstodays(date2[1]) + date2[0]
+
+days = if days2 > days1
+         days2 - days1 - 1
+       else
+         days1 - days2 - 1
+       end
+
+days = 0 if days.negative?
+
+puts days
